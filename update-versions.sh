cfeab29d47b6fa8e7a7c35e18efc83ae6921ab15
engels74 <141435164+engels74@users.noreply.github.com>
2025-01-19 14:33:20 +0100

    feat: nightly build [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/sct/overseerr/commits/develop" | jq -re .sha) || exit 1
+version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/engels74/overseerr-anime-source/commits/feature-default-anime-instance-checkbox-nightly" | jq -re .sha) || exit 1
 [[ -z ${version} ]] && exit 0
 [[ ${version} == null ]] && exit 0
-message=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/sct/overseerr/commits/${version}" | jq -re .commit.message) || exit 1
+message=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/engels74/overseerr-anime-source/commits/${version}" | jq -re .commit.message) || exit 1
 [[ ${message} == *"[skip ci]"* ]] && exit 0
 json=$(cat VERSION.json)
 jq --sort-keys \
